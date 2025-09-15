3624110aa614cdc1fdef468c22342b9a50193719
Kipyegon Wilber <kenmisik@gmail.com>
2025-09-15 21:15:38 +0300

    Login endpoint working

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 import prisma from '../prismaClient.js';
-
+import jwt from "jsonwebtoken"
 
 // Register user
 export async function registerUser(req, res) {
@@ -21,6 +21,46 @@ export async function registerUser(req, res) {
         res.status(500).json({ error: "Something went wrong" });
     }
 }
+
+// login
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // find user
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      return res.status(400).json({ error: "Invalid email or password" });
+    }
+
+    // compare password
+    const isValid = await bcrypt.compare(password, user.passwordHash);
+    if (!isValid) {
+      return res.status(400).json({ error: "Invalid email or password" });
+    }
+
+    // sign JWT
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET || "supersecret", // better to set in .env
+      { expiresIn: "1h" }
+    );
+
+    res.json({
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Login failed" });
+  }
+};
+
+
 export async function getUser(req, res) {
     try {
         const id = Number(req.params.id);
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,14 +1,17 @@
 import { Router } from 'express';
 const router = Router();
-import { registerUser, getUser, listUsers } from '../controllers/userController.js';
+import { registerUser, getUser, listUsers, loginUser } from '../controllers/userController.js';
 
 // create a user
-router.post('/', registerUser);
+router.post('/users', registerUser);
 
 // list all users 
 router.get('/', listUsers);
 
 // get a single user
 router.get('/:id', getUser);
+
+// new login endpoint
+router.post("/login", loginUser);
 
 export default router; 
